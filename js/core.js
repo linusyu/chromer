8099661c1d8973cfa6fb2cdf1e2dad7cb0c8cc73
linusyu <ilikethisgames@gmail.com>
2015-01-06 19:39:20 +0800

    init

---
+++ js/core.js
@@ -0,0 +1,58 @@
+
+"use strict";
+
+(function(){
+
+    window.nch = {};
+
+    nch.dom = {
+        tpl : document.querySelector("#template"),
+        cmt : document.querySelector("#container")
+    }
+
+    nch.ajax = function (details, onload) {
+        if (typeof details === 'string') details = {
+            'url': details
+        };
+        if (onload) details.onload = onload;
+        details.method = details.method || 'GET';
+        details.body = details.body || '';
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (details.onreadystatechange) details.onreadystatechange(xhr);
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    if (details.onload) details.onload.call(xhr);
+                } else {
+                    console.error('Network fail: [%s] %s', details.method, details.url);
+                    if (details.onerror) details.onerror.call(xhr);
+                }
+            }
+        }
+        xhr.open(details.method, details.url, true);
+        xhr.responseType = "text";
+        xhr.send(details.body);
+    };
+})();
+(function(){
+    var list = document.querySelectorAll("#list a");
+    [].forEach.call(list,function(i){
+        i.addEventListener("click", function(){
+            nch.dom.tpl.querySelector("h1").textContent = i.textContent;
+            nch.dom.tpl.style.display = "block";
+            nch.ajax({
+                url:"template/" + i.dataset.url + ".html",
+                onload: function(){
+                    var dom = new DOMParser().parseFromString(this.responseText, 'text/html');
+                    nch.dom.cmt.innerHTML = dom.body.innerHTML;          
+                }
+            });            
+        },false)
+    });
+    var close = document.querySelector("#close a");
+    close.addEventListener("click", function(){
+        nch.dom.tpl.style.display = "none";
+        nch.dom.cmt.innerHTML = "";
+    }, false)    
+})();
